432c542281e3048360ca7d69932bfab830a5af12
PixelPerfectDev <joshbowdenconcepts@gmail.com>
2021-05-04 21:57:14 -0400

    Added formatting and linting - ESLint for linting - Prettier for formating - Formatted code

--- NOTES.md
+++ NOTES.md
@@ -2,3 +2,4 @@
 
 - Creating react and typescript from scratch based off of this video: https://www.youtube.com/watch?v=Elpu7CIuqjY
 - Babel is used to convert the typescript into javascript that can be understood by the browser
+- Webpack is used to compile the files and make them work for their respective environments
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import "./styles.css";
+import { ClickCounter } from './ClickCounter'
+import './styles.css'
 
 export const App = () => {
   return (
     <>
-      <h1>React Typescript</h1>
+      <h1>React Typescript Application</h1>
+      {/* It might be good to have a banner for this on development so that I don't make any mistakes on the platforms */}
+      <p>Current Environment: {process.env.name}</p>
+      <ClickCounter />
     </>
-  );
-};
+  )
+}
