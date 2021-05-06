f5fca18ecd3c64296e815af767ddbd0eac4d25fb
PixelPerfectDev <joshbowdenconcepts@gmail.com>
2021-05-05 20:12:53 -0400

    FITR-3 Setup component based testing with jest

---
+++ src/App.spec.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, configure } from 'enzyme'
+
+import { App } from './App'
+
+configure({ adapter: new Adapter() })
+describe('<App />', () => {
+    it('should contain a layout component', () => {
+        const wrapper = shallow(<App />)
+        expect(wrapper.find('Layout')).toHaveLength(1)
+    })
+})
